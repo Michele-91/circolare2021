a7e7c9af099d9243ca04920a24b8d7b2e0963e7d
Michele Spinelli <michele.spinelli@reti.it>
2021-06-11 15:58:39 +0200

    migliorata validazione per creazione sala

--- src/web/ts/salaCrea.ts
+++ src/web/ts/salaCrea.ts
@@ -26,48 +26,47 @@ function associaSalaAdEdificio(response) {
     }
     for (let edificio of listaEdifici) {
         $("#opzioni-edificio-per-sala").append(
-            `<option value=${edificio.nome}>`
+            `<option value="${edificio.nome}">`
         );
     }
-    console.log($("#opzioni-edificio-per-sala")[0]);
-    $("input[name=opzioni-edificio-per-sala]").on('change', function () {
-        let result = $(this).val();
-        if (result) {
-            submitSala(result, listaEdifici);
-        } else {
-            alert("Devi selezionare un edificio");
-        }
-    });
+    creaSala(listaEdifici);
 }
 
-function submitSala(edificioSelezionato: any, edificiEsitenti: any[]) {
-    $('#submit_sala').on('click', (e) => {
+function creaSala(edificiEsistenti) {
+
+    $('#submit_sala').on('click', (e,) => {
         e.preventDefault();
         let nome: string;
         let edificioId: number;
-        edificioId = edificiEsitenti.filter(e => e.nome == edificioSelezionato)[0].id;
-        console.log("edificioId: " + edificioId);
-        e.preventDefault()
         nome = $('#nome_sala').val().toString();
-        if (!nome) {
-            alert("Devi selezionare un nome");
-            return;
+        let edificioSelezionato = $("input[name=opzioni-edificio-per-sala]").val();
+        console.log("valore edificio: " + edificioSelezionato);
+        if (nome && edificioSelezionato) {
+            let edificioPresenteNelDatabase = edificiEsistenti.some(ed => ed.nome == edificioSelezionato);
+            if(edificioPresenteNelDatabase) {
+                edificioId = edificiEsistenti.filter(e => e.nome == edificioSelezionato)[0].id;
+                console.log("edificioId: " + edificioId);
+                let obj = {
+                    nome: nome,
+                    edificioId: edificioId
+                }
+                $.ajax({
+                    url: "http://localhost:42877/api/Sala/Add",
+                    contentType: "application/json",
+                    type: "POST",
+                    dataType: "json",
+                    data: JSON.stringify(obj)
+                }).done(function (response) {
+                    window.location.href = './sala_mostra.html';
+                }).fail(function (xhr) {
+                    alert("Errore: non è stato possibile creare la sala!");
+                    console.log(`Error - ${xhr.statusText} (${xhr.status}): ${xhr.responseText}`);
+                })
+            } else {
+                alert("Non puoi creare una sala in un edificio inesistente"); 
+            }
+        } else {
+            alert("Devi riempire tutti i campi per poter effettuare l'operazione");
         }
-        let obj = {
-            nome: nome,
-            edificioId: edificioId
-        }
-        $.ajax({
-            url: "http://localhost:42877/api/Sala/Add",
-            contentType: "application/json",
-            type: "POST",
-            dataType: "json",
-            data: JSON.stringify(obj)
-        }).done(function (response) {
-            window.location.href='./sala_mostra.html';
-        }).fail(function (xhr) {
-            alert("Errore: non è stato possibile creare la sala!");
-            console.log(`Error - ${xhr.statusText} (${xhr.status}): ${xhr.responseText}`);
-        })
     })
 }
